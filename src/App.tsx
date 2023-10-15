7b990f59f7b1af670559aaa6bed86b3b7b496c2a
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-10-15 13:15:44 -0400

    add-material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Box, CircularProgress } from "@mui/material";
+
 import { useFetch } from "./hooks";
 
 function App() {
   const { data, loading, error } = useFetch();
   console.log({ data, loading, error });
-  return <>Hello</>;
+  return loading ? <CircularProgress /> : <Box>Hello World</Box>;
 }
 
 export default App;
