3840b31f08acab750a61c970d9c7187f8d24557d
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-10-16 10:54:42 -0400

    add grid add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { Box, CircularProgress } from "@mui/material";
+import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Header } from "./Components";
 
 import { useFetch } from "./hooks";
 
 function App() {
-  const { data, loading, error } = useFetch();
-  console.log({ data, loading, error });
-  return loading ? <CircularProgress /> : <Box>Hello World</Box>;
+  // const { data, loading, error } = useFetch();
+
+  return (
+    <div style={{ margin: 0 }}>
+      <Header />
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <SideNave />
+        </Grid>
+        <Grid item xs={10}>
+          <Content />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
+
+const SideNave = () => (
+  <Box sx={{ width: "100%", height: "91vh", backgroundColor: "pink" }}></Box>
+);
+
+const Content = () => (
+  <Box
+    sx={{ width: "100%", height: "91vh", backgroundColor: "lightblue" }}
+  ></Box>
+);
+
