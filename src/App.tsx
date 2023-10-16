aa544e1e9b763c4486111967218c7d72e4d7ba6c
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-10-16 15:36:40 -0400

    work on getting state management up and running

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import { Reducer, useReducer, createContext } from "react";
 import { ResponsiveDrawer } from "./Components";
-
 import { useFetch } from "./hooks";
 
+// const AppContext = createContext();
+
+export enum ActionType {
+  SET_DATA = "SET_DATA",
+  REMOVE_DATA = "REMOVE_DATA",
+}
+
+interface Actions {
+  type: ActionType;
+  payload: {
+    data: [];
+  };
+}
+
+interface InitialState {
+  data: [];
+}
+
+const stateReducer: Reducer<InitialState, Actions> = (
+  state: [],
+  action: Actions
+) => {
+  switch (action.type) {
+    case "SET_DATA":
+      return action.payload.data;
+    case "REMOVE_DATA":
+      return [];
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [data, dispatch] = useReducer(stateReducer, []);
   // const { data, loading, error } = useFetch();
 
   return (
@@ -13,4 +46,3 @@ function App() {
 }
 
 export default App;
-
