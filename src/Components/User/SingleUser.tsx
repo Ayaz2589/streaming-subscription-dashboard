4e28e5a4ec26494852022ea137e40ed89b2cda81
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-10-18 12:11:50 -0400

    add back button

--- src/Components/User/SingleUser.tsx
+++ src/Components/User/SingleUser.tsx
@@ -2,13 +2,23 @@ import { useLocation } from "react-router-dom";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 const SingleUser = () => {
   const location = useLocation();
   const { selectedUser } = location.state;
+  const navigate = useNavigate();
   console.log(selectedUser);
   return (
     <Box>
+      <Button
+        onClick={() => {
+          navigate("/users");
+        }}
+      >
+        Back
+      </Button>
       <Paper elevation={0} sx={{ backgroundColor: "#eee" }}>
         <Box sx={{ display: "flex" }}>
           <img src={selectedUser.avatar} height={150} width={150} />
