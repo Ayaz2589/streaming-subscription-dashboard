8d30185d775f5bffa77f3c505860d290d348d858
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-10-18 11:58:41 -0400

    add top section

--- src/Components/User/SingleUser.tsx
+++ src/Components/User/SingleUser.tsx
@@ -1,10 +1,34 @@
 import { useLocation } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const SingleUser = () => {
   const location = useLocation();
   const { selectedUser } = location.state;
   console.log(selectedUser);
-  return <div>Hello</div>;
+  return (
+    <Box>
+      <Paper elevation={0} sx={{ backgroundColor: "#eee" }}>
+        <Box sx={{ display: "flex" }}>
+          <img src={selectedUser.avatar} height={150} width={150} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignSelf: "center",
+            }}
+          >
+            <Typography variant="h4">
+              {selectedUser.personal_information.fullName}
+            </Typography>
+            <Typography variant="h5">{selectedUser.email}</Typography>
+            <Typography variant="h6">{`User ID: ${selectedUser.username}`}</Typography>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
+  );
 };
 
 export default SingleUser;
