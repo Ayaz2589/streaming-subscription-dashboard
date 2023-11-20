48ffda141040ad9efbf4d41cb90a5c0462d00af1
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-11-20 11:43:21 -0500

    add theme file

--- src/App.tsx
+++ src/App.tsx
@@ -3,72 +3,8 @@ import {
   DashboardContextProvider,
 } from "./context/DashboardContext";
 import { Main } from "./Components/v1";
-import {
-  createTheme,
-  colors,
-  ThemeProvider,
-  responsiveFontSizes,
-} from "@mui/material";
-
-declare module "@mui/material/styles" {
-  interface Palette {
-    neutral: Palette["primary"];
-  }
-
-  interface PaletteOptions {
-    neutral?: PaletteOptions["primary"];
-  }
-}
-
-let theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 950,
-      lg: 2150,
-      xl: 2500,
-    },
-  },
-  components: {
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          "&.subvariant-hovered": {
-            "& fieldset": {
-              border: "none",
-            },
-          },
-        },
-      },
-    },
-    MuiButtonBase: {
-      defaultProps: {
-        disableRipple: true,
-      },
-    },
-  },
-  palette: {
-    primary: {
-      main: colors.deepPurple[500],
-      light: colors.deepPurple[50],
-      dark: colors.deepPurple[900],
-    },
-    neutral: {
-      main: colors.blueGrey[500],
-      light: colors.blueGrey[50],
-      dark: colors.blueGrey[900],
-    },
-  },
-  typography: {
-    fontFamily: ["Lato"].join(","),
-  },
-  shape: {
-    borderRadius: 16,
-  },
-});
-
-theme = responsiveFontSizes(theme);
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
 
 function App() {
   return (
