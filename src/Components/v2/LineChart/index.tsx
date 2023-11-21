94c4c5be501e8f77673cd048e4e28cb842f94c02
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-11-20 20:46:25 -0500

    work on selector

--- src/Components/v2/LineChart/index.tsx
+++ src/Components/v2/LineChart/index.tsx
@@ -1,7 +1,7 @@
+import { useState } from "react";
 import {
   LineChart as LineChartRecharts,
   XAxis,
-  YAxis,
   Tooltip,
   Line,
   ResponsiveContainer,
@@ -9,8 +9,10 @@ import {
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import theme from "../../../theme";
 
 const Card = styled(Paper)(({ theme }) => ({
@@ -45,82 +47,160 @@ const data = [
   },
 ];
 
+const Legend = () => (
+  <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+    <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          width: "20px",
+          height: "20px",
+          borderRadius: "50%",
+          marginRight: "1rem",
+          backgroundColor: theme.palette.primary.main,
+        }}
+      ></Box>
+      <Typography variant="body2">Sales</Typography>
+    </Box>
+    <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          width: "20px",
+          height: "20px",
+          borderRadius: "50%",
+          marginRight: "1rem",
+          backgroundColor: theme.palette.secondary.main,
+        }}
+      ></Box>
+      <Typography variant="body2">Earnings</Typography>
+    </Box>
+  </Box>
+);
+
+const Header = () => (
+  <Box
+    sx={{
+      display: "flex",
+      justifyContent: "space-between",
+      padding: "1rem",
+    }}
+  >
+    <Typography variant="h6">Sales vs Earnings</Typography>
+    <ControlledOpenSelect />
+  </Box>
+);
+
+const ControlledOpenSelect = () => {
+  const [age, setAge] = useState<string | number>("Quarterly");
+  const [open, setOpen] = useState(false);
+
+  const handleChange = (event: SelectChangeEvent<typeof age>) => {
+    setAge(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          size="small"
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          defaultValue={age}
+          onChange={handleChange}
+        >
+          <MenuItem value={"monthly"} disabled>
+            Monthly
+          </MenuItem>
+          <MenuItem value={"quarterly"} selected={true}>
+            Quarterly
+          </MenuItem>
+          <MenuItem value={"yearly"} disabled>
+            Yearly
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
+
+const Chart = () => (
+  <ResponsiveContainer width="100%" aspect={1.5}>
+    <LineChartRecharts
+      width={500}
+      height={500}
+      data={data}
+      margin={{
+        top: 15,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <XAxis
+        dataKey="name"
+        axisLine={false}
+        tickSize={0}
+        tick={{ fill: theme.palette.neutral.light }}
+      />
+      <Tooltip
+        contentStyle={{
+          backgroundColor: theme.palette.primary.light,
+          color: theme.palette.neutral.dark,
+          borderRadius: "0.5rem",
+          border: "none",
+          fontWeight: "bold",
+        }}
+        itemStyle={{ color: theme.palette.neutral.dark }}
+        cursor={true}
+        formatter={(value: string) => `$${value}`}
+      />
+      <Line
+        type="monotone"
+        dataKey="sales"
+        stroke={theme.palette.primary.main}
+        strokeWidth="5"
+        dot={{ r: 0 }}
+        activeDot={{
+          fill: theme.palette.primary.main,
+          stroke: theme.palette.primary.main,
+          strokeWidth: 5,
+          r: 6,
+        }}
+      />
+      <Line
+        type="monotone"
+        dataKey="earnings"
+        stroke={theme.palette.secondary.main}
+        strokeWidth="5"
+        dot={{ r: 0 }}
+        activeDot={{
+          fill: theme.palette.secondary.main,
+          stroke: theme.palette.secondary.main,
+          strokeWidth: 5,
+          r: 6,
+        }}
+      />
+    </LineChartRecharts>
+  </ResponsiveContainer>
+);
+
 const LineChart = () => {
   return (
     <Card elevation={0} sx={{ display: "flex", flexDirection: "column" }}>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "1rem",
-        }}
-      >
-        <Button variant="contained">Month</Button>
-        <Button variant="outlined">Chart</Button>
-      </Box>
+      <Header />
       <Box sx={{ display: "flex" }}>
-        <Box>
-          <Typography variant="body2">Total Spent</Typography>
-          <Typography variant="h5">$37.5K</Typography>
-        </Box>
-        <ResponsiveContainer width="100%" aspect={1.5}>
-          <LineChartRecharts
-            width={500}
-            height={500}
-            data={data}
-            margin={{
-              top: 15,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <XAxis
-              dataKey="name"
-              axisLine={false}
-              tickSize={0}
-              tick={{ fill: theme.palette.neutral.light }}
-            />
-            <Tooltip
-              contentStyle={{
-                backgroundColor: theme.palette.primary.light,
-                color: theme.palette.neutral.dark,
-                borderRadius: "0.5rem",
-                border: "none",
-                fontWeight: "bold",
-              }}
-              itemStyle={{ color: theme.palette.neutral.dark }}
-              cursor={true}
-              formatter={(value: string) => `$${value}`}
-            />
-            <Line
-              type="monotone"
-              dataKey="sales"
-              stroke={theme.palette.primary.main}
-              strokeWidth="5"
-              dot={{ r: 0 }}
-              activeDot={{
-                fill: theme.palette.primary.main,
-                stroke: theme.palette.primary.main,
-                strokeWidth: 5,
-                r: 6,
-              }}
-            />
-            <Line
-              type="monotone"
-              dataKey="earnings"
-              stroke={theme.palette.secondary.main}
-              strokeWidth="5"
-              dot={{ r: 0 }}
-              activeDot={{
-                fill: theme.palette.secondary.main,
-                stroke: theme.palette.secondary.main,
-                strokeWidth: 5,
-                r: 6,
-              }}
-            />
-          </LineChartRecharts>
-        </ResponsiveContainer>
+        <Legend />
+        <Chart />
       </Box>
     </Card>
   );
