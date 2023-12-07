91e6a8777afbe7bae9a1de7fb90c6cdaa72850aa
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-12-07 16:26:46 -0500

    add dark mode to cards

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,14 +1,18 @@
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import { useDarkMode } from "../../context";
 
-const Card = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  minHeight: "5rem",
-  boxShadow: theme.shadows[1],
-}));
+const Card = styled(Paper)(({ theme }) => {
+  const { isDarkMode } = useDarkMode();
+  return {
+    backgroundColor: isDarkMode ? theme.palette.neutral.main : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    minHeight: "5rem",
+    boxShadow: theme.shadows[1],
+  };
+});
 
 export default Card;
