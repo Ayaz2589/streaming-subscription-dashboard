d9a2770f7a4f2bae014c104597c8ad0d115f93b8
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-01-29 16:30:30 -0500

    work on setting fetch environment

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://app-backend-62sfwjhbj-ayaz2589.vercel.app",
+const environment = import.meta.env.MODE
+console.log(`Environment is: ${environment}`)
+
+const axiosOptions = {
+  baseUrl: environment === "development" ? "http://localhost:8080" : "https://app-backend-62sfwjhbj-ayaz2589.vercel.app",
   headers: {
     "Content-type": "application/json",
   },
-});
+}
+
+console.log(axiosOptions.baseUrl)
+
+const api = axios.create(axiosOptions);
 
 export default api;
