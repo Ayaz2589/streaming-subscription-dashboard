eece16887b6039c7d4455a7b174a03243bcefdf0
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-02-14 17:40:06 -0500

    use location pathname for selected button

--- src/Components/DashboardDrawer/index.tsx
+++ src/Components/DashboardDrawer/index.tsx
@@ -13,7 +13,7 @@ import { DashboardIcon, ProjectIcon } from "../../svg";
 import { UserSettings } from "..";
 import { SideNavItems } from "../../enums";
 import { useTheme } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useDarkMode } from "../../context";
@@ -31,6 +31,9 @@ const DashboardDrawer = ({ window, mobileOpen, handleDrawerToggle }: Props) => {
   const theme = useTheme();
   const navigate = useNavigate();
   const { isDarkMode } = useDarkMode();
+  const location = useLocation();
+
+  console.log(location.pathname);
 
   const SideNavItemsArray = [
     {
@@ -140,7 +143,7 @@ const DashboardDrawer = ({ window, mobileOpen, handleDrawerToggle }: Props) => {
           <List sx={{ marginTop: "3rem" }}>
             {SideNavItemsArray.map((item) => {
               const { index, selected, notSelected, section } = item;
-              const isSelected = false;
+              const isSelected = location.pathname === item.path;
               return (
                 <ListItem key={index} disablePadding>
                   <ListItemButton
